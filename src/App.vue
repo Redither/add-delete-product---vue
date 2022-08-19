2a68efc9d2e38d86e56e85d4e923b345a1148c07
Redither <slavonchik68@gmail.com>
2022-08-19 14:56:34 +0300

    Created base app. Start prod

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ProductManager from './components/pages/ProductManaGer.vue';
+</script>
+
+<template>
+  <ProductManager/>
+</template>
+
+<style>
+@import url(./assets/CSS/main.css);
+</style>
---
+++ src/components/pages/ProductManager.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AddProduct from "../Forms/AddProduct.vue";
+
+</script>
+
+<template>
+<section>
+    <h1>Добвление товара</h1>
+    <AddProduct/>
+</section>
+</template>
